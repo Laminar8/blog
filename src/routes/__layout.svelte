01afc41592d7825fe9ff22efda072bda75200156
laminar <laminar@kakao.com>
2022-08-02 20:42:19 +0900

    The last time with the image cursor

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
 	import '../app.scss';
+	import Cursor from '$lib/cursor/cursor.svelte';
 </script>
 
-<slot />
+<main>
+	<Cursor />
+	<div class="onboard">
+		<slot />
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		/* 시스템 커서 안 보이게 숨기기 */
+		cursor: none;
+
+		height: 100%;
+		width: 100%;
+
+		// https://ccuram.tistory.com/27
+		// https://developer.mozilla.org/ko/docs/Web/CSS/box-sizing
+		padding: 5vh 2vw;
+		box-sizing: border-box;
+		background-color: var(--color-dark);
+	}
+
+	.onboard {
+		height: 100%;
+		width: 100%;
+
+		box-shadow: 0 0 0 2rem var(--color-white-border) inset;
+		border-radius: 3rem;
+
+		padding: 2.1rem;
+		box-sizing: border-box;
+
+		// Split sections
+		display: grid;
+		grid-template-rows: 1fr 19fr;
+	}
+</style>
