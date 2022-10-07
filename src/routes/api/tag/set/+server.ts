c113f3f6cd2920d0fbdd47a3e38915a69bef49a9
laminar <laminar@kakao.com>
2022-10-08 02:11:22 +0900

    netlify

--- src/routes/api/tag/set/+server.ts
+++ src/routes/api/tag/set/+server.ts
@@ -1,6 +1,7 @@
 import { tag } from '$lib/contents/store';
+import type { RequestEvent } from '@sveltejs/kit';
 
-export async function POST(request: Request) {
+export async function POST({ request }: RequestEvent) {
 	const req = await request.json();
 	tag.update(() => req.tag);
 
