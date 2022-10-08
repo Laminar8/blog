5c333b9d09070bc1c77db665002c1286d62ee21a
laminar <laminar@kakao.com>
2022-10-08 14:24:19 +0900

    netlify

--- src/lib/contents/url.ts
+++ src/lib/contents/url.ts
@@ -1,3 +1,3 @@
 // When deploying, url needs to be changed to '/blog'
 // When dev, url needs to be changed to ''
-export const url = process.env.NODE_ENV === 'development' ? '' : '/blog';
+export const url = process.env.NODE_ENV === 'development' ? '' : '';
